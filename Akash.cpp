141a79ec115cd487fa899fbcc92a2f2ff7cea2cc
Aditya Vikram <46237362+avikram553@users.noreply.github.com>
2019-10-19 15:35:21 +0530

    code added code:- FLOW013

---
+++ Akash.cpp
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------------
+//CODECHEF :-vALID triangles
+//code :- FLOW013
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int a,b,c;
+		cin>>a>>b>>c;
+		
+		if(a+b+c==180)
+		cout<<"YES\n";
+		else
+		cout<<"NO\n";
+	}
+	return 0;
+}
